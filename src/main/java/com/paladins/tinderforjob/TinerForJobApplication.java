c89fede4a42901191a0e77d54a0fd4c165fbdd3e
Nana <real_pcy@list.ru>
2022-10-19 16:52:42 +0500

    init commit

---
+++ src/main/java/com/paladins/tinderforjob/TinerForJobApplication.java
@@ -0,0 +1,13 @@
+package com.paladins.tinderforjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinerForJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinerForJobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paladins/tinderforjob/TinerForJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paladins.tinderforjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinerForJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
